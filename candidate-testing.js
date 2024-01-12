ad8759b2a065e988842cff0d56a7912f05809943
Roger Thomas <rogerthomas96@outlook.com>
2024-01-12 15:16:58 -0600

    Completed 'Candidate Testing' assignment and fixed small logic error

--- candidate-testing.js
+++ candidate-testing.js
@@ -19,12 +19,12 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  let candidateName = input.question("Enter your name: ");
+  candidateName = input.question("Enter your name: ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  let candidateAnswer = input.question(question);
+  candidateAnswer = input.question(question);
 }
 
 function gradeQuiz(candidateAnswers) {
@@ -46,7 +46,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log( "Hello," + candidateName);
+   console.log( "Hello, " + candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
